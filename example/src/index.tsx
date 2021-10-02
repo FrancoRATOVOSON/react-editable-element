05f23a6f6b5c5ba707f7641d3bc534a3e43061c8
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2021-10-02 21:52:08 +0200

    move to yarn 1

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,10 +2,12 @@ import ReactDom from 'react-dom'
 import React from 'react'
 
 import './styles.css'
+import EditableElement from '../../lib/esm'
 
 function App() {
   return (
     <div className={'container'}>
+      <EditableElement name={`RJ Franco`}/>
     </div>
   )
 }
