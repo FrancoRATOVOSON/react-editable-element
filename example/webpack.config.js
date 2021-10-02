f0c3045ed9b662068c83c313f7bc9100f9b74249
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2021-10-02 19:30:58 +0200

    Created new react projectfor test

---
+++ example/webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
+const webpack = require("webpack");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|yarn|bower_components)/,
+        options: {
+          presets: ["@babel/env"]
+        }
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public/index.html'),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new NodePolyfillPlugin()
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname , 'dist'),
+    clean: true
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    port: 3000
+  },
+};
